aa79209cdf73cb44273b7b1954e1dd25a0ee6822
Rodrigo Inoue <pt234989@IFSPROOT.PTB>
2022-04-11 20:22:29 -0300

    Trying to make a movie card

--- RN2_Desafio2/App.js
+++ RN2_Desafio2/App.js
@@ -1,12 +1,24 @@
+import React, { useEffect, useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Welcome from './movie';
+import Movie from './movie';
+import api from './services/api';
+import { createPortal } from "react-dom";
 
 export default function App() {
+  // const [movies, setMovies] = useState(null)
+  // const apiConsume = async () => {
+  //   const response = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=51e0824a`);
+  //   const data = await response.json();
+  //   return data;
+  // };
+  // useEffect(() => {
+  //   console.log(apiConsume().then(data => {setMovies(data)}))
+  //   console.log(movies.Title)
+  // }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Welcome name="Teste"/>
+      <Movie/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +27,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- RN2_Desafio2/movie.js
+++ RN2_Desafio2/movie.js
@@ -1,6 +1,39 @@
-import {Text} from 'react-native'
-function Welcome(props) {
-    return <Text>Olá, {props.name}</Text>;
+import React from 'react'
+import {Text, Image, View, Button, StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    card: {
+      width:500,
+      height:500,
+      backgroundColor: 'pink',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+function Movie(props) {
+    return( 
+        <View styles={styles.card}>  
+            {/* Image */}
+            <View>
+                <Image
+                source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+                />
+            </View>
+            {/* Name and Button */}
+            <View>
+                <View>
+                    <Text>{props.Title}</Text>
+                </View>
+                <View>
+                    <Button
+                        title="Press me!"
+                    />
+                </View>
+            </View>
+        </View>
+    )
 }
 
-export default Welcome
+export default Movie
+
