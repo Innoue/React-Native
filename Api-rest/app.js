94fb6fa3179e37bfcf2fa20cb0093cbd973eb575
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-05-10 19:29:36 -0300

    Adeed some validators

--- Api-rest/app.js
+++ Api-rest/app.js
@@ -4,7 +4,7 @@ const fs = require('fs/promises')
 const jwt = require('jsonwebtoken')
 const userSchema = require('./userSchema')
 require('./mongoose')()
-
+const {body, validationResult} = require('express-validator')
 
 app.use(express.json())
 
@@ -28,7 +28,7 @@ app.use((req,res,next) =>{
   }
 })
 
-app.get('/auth',async(req,res) => {
+app.post('/auth',async(req,res) => {
   const {user, password} = req.body
 
   const userExists = await userSchema.findOne({
@@ -50,17 +50,35 @@ app.get('/users',async(req,res) => {
 
 app.get('/users/:id',async(req,res) => {
   const user = await userSchema.findById(req.params.id)
+  if(!user) 
+    return res.status(404).json({message:"User not found"})
   res.status(200).json(user)
 })
 
-app.post('/users',async(req,res) => {
-  const user = await userSchema.create(req.body)
-  res.status(200).json(user)
+app.post('/users',[
+  body('user').notEmpty(),
+  body('password').isLength({min: 8})
+],
+  async(req,res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty())
+      return res.status(400).json({error: errors.array()})
+    const{user, password} = req.body
+    const userS = await userSchema.create({user, password})
+    res.status(200).json(userS)
 })
 
-app.put('/users/:id',async(req,res) => {
-  const user = await userSchema.findByIdAndUpdate(req.params.id, req.body)
-  res.status(200).json(user)
+app.put('/users/:id',[
+  body('user').notEmpty(),
+  body('password').isLength({min: 8})
+],
+async(req,res) => {
+  const errors = validationResult(req)
+  if(!errors.isEmpty())
+    return res.status(400).json({error: errors.array()})
+  const{user, password} = req.body
+  const userS = await userSchema.findByIdAndUpdate(req.params.id, {user, password}, {new: true})
+  res.status(200).json(userS)
 })
 
 app.delete('/users/:id',async(req,res) => {
