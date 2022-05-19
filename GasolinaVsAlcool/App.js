37221cec07192c4e8f7bb456a30d161c7d209575
Innoue <rodrigoinoue1122@gmail.com>
2022-05-19 15:38:30 -0300

    Front end done, need to fix button Calcular

--- GasolinaVsAlcool/App.js
+++ GasolinaVsAlcool/App.js
@@ -1,11 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Image,Text, View, Modal, Button, TextInput, TouchableOpacity } from 'react-native';
+import GasVsAlcool from './src/Modal';
 
 export default function App() {
+  const [visible, setVisible] = useState(false)
+
+  function entrar(){
+    setVisible(true)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./src/img/logo.png')}
+        style={styles.img}
+      />
+      <Text style={styles.title}>
+        Qual melhor opção?
+      </Text>
+      <View style={styles.containerHint}>
+        <Text style={styles.hint}>
+          Álcool (preço por litro):
+        </Text>
+      </View>
+      <View style={styles.containerInput}>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+        />
+      </View>
+      <Text style={styles.hint}>
+        Gasolina (preço por litro):
+      </Text>
+      <View style={styles.containerInput}>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+        />
+      </View>
+      <TouchableOpacity 
+        onPress={entrar}
+        style={[styles.containerInput]} 
+      >
+        <View style={styles.touchable}>
+          <Text style={styles.title}>
+            Calcular
+          </Text>
+        </View>
+      </TouchableOpacity>
+      <Modal
+        animationType='slide'
+        transparent={true}
+        visible={visible}
+      >
+        <GasVsAlcool close={() => setVisible(false)}/>
+      </Modal>
+      {/* <Button
+        title='Aperte'
+        
+      /> */}
     </View>
   );
 }
@@ -13,8 +66,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#282424',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  img:{
+    height: 168,
+    width: 168,
+  },
+  title:{
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30,
+    paddingVertical: 20,
+  },
+  hint:{
+    color: 'white',
+    fontSize: 22,
+    textAlign: 'left'
+  },
+  input:{
+    backgroundColor: 'white',
+    flex: 1,
+    height: 50,
+    marginHorizontal: 15,
+    marginVertical: 10,
+    paddingHorizontal: 15,
+    borderRadius: 10
+  },
+  containerInput:{
+    flexDirection: 'row'
+  },
+  containerHint:{
+    
+  },
+  touchable:{
+    backgroundColor: '#f04434',
+    flex: 1,
+    alignItems: 'center',
+    marginHorizontal: 15,
+    marginVertical: 10,
+    borderRadius: 10
+  }
 });
---
+++ GasolinaVsAlcool/src/Modal/index.js
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet, View, Image, TouchableOpacity, Button } from 'react-native';
+export default function GasVsAlcool(props) {
+  function sair(){
+    
+  }
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../img/gas.png')}
+        style={styles.img}
+      />
+      <Text style={styles.greenText}>
+        Compensa usar @variavel
+      </Text>
+      <View style={styles.containerText}>
+        <Text style={[styles.textBold, styles.text]}>Com os preços:</Text>
+        <Text style={styles.text}>Álcool: R$ price</Text>
+        <Text style={styles.text}>Gasolina: R$ price</Text>
+      </View>
+      <TouchableOpacity 
+        style={{flexDirection: 'row'}}
+        onPress={props.close}
+      >
+        <View style={styles.input}>
+          <Text style={[styles.text, styles.textRed]}>Calcular novamente</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    backgroundColor: '#282424',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  img:{
+    height: 168,
+    width: 168,
+    marginBottom: 20
+  },
+  greenText:{
+    color: '#30ef5a',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  containerText:{
+    padding: 20,
+    marginVertical: 20,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text:{
+    color: 'white',
+    fontSize: 22,
+    padding: 2
+  },
+  textBold:{
+    fontWeight: 'bold'
+  },
+  input:{
+    flex: 1,
+    marginHorizontal: 30,
+    borderRadius: 10,
+    borderWidth: 1.5,
+    borderColor: '#f04434',
+    padding: 15,
+    alignItems: 'center'
+  },
+  textRed:{
+    color: '#f04434',
+  }
+})
