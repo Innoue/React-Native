0e85033ab68be8ba40edb6b6910f3f5b9d864cd3
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-06-06 18:31:19 -0300

    Created project with dependencies

---
+++ Login/src/firebaseConnection.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaU6CwHgDnwJWE7s33nqFi2N8Xl9-7a_0",
+  authDomain: "fir-app-969f5.firebaseapp.com",
+  databaseURL: "https://fir-app-969f5.firebaseio.com",
+  projectId: "fir-app-969f5",
+  storageBucket: "fir-app-969f5.appspot.com",
+  messagingSenderId: "410115304411",
+  appId: "1:410115304411:web:578d63b8fef94216ae15fe"
+};
+
+if(!firebase.getApps.length){
+  //Open connection
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
