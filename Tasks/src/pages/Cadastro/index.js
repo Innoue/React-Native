c35bc34a9c4c9d8117eeae0c2274c14c685f5fcc
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-06-08 08:45:10 -0300

    Front end login screens

--- Tasks/src/pages/Cadastro/index.js
+++ Tasks/src/pages/Cadastro/index.js
@@ -1,11 +1,85 @@
-import React from "react";
-import { View, Text } from "react-native";
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import firebase from "../../services/firebaseConnection"
 
 export default function Cadastro(){
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation()
+
+  function goToLogin(){
+    navigation.navigate('Login')
+  }
+
   return(
-    <View>
-      <Text>Cadastro</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="email@email.com"
+        underlineColorAndroid="transparent"
+        onChangeText={(text) => setEmail(text)}
+        keyboardType='email-address'
+        value={email}
+      />
+      <Text style={styles.text}>Senha</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="*********"
+        underlineColorAndroid="transparent"
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+        value={password}
+      />
+      <View style={styles.areaBtn}>
+        <TouchableOpacity 
+          style={styles.handleLogin}
+          onPress={() => {}}
+        >
+          <Text style={styles.textBtn}>Cadastrar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={goToLogin}
+        >
+          <Text style={{textAlign: 'center', fontSize: 20, marginTop: 15}}>Já possuo uma conta</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fffafa',
+    paddingTop: 20,
+    paddingHorizontal: 20
+  },
+  text:{
+    fontSize: 22
+  },
+  input:{
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#171614',
+    borderRadius: 5,
+    backgroundColor: '#FFF',
+    height: 50
+  },
+  handleLogin:{
+    marginTop: 10,
+    backgroundColor: '#171614',
+    justifyContent: 'center',
+    height: 50,
+    borderRadius: 10
+  },
+  textBtn:{
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 22
+  },
+  
+});
--- Tasks/src/pages/Login/index.js
+++ Tasks/src/pages/Login/index.js
@@ -1,13 +1,85 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 import firebase from "../../services/firebaseConnection"
 
-
 export default function Login(){
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const navigation = useNavigation()
+
+  function goToCadastro(){
+    navigation.navigate('Cadastro')
+  }
+
   return(
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="email@email.com"
+        underlineColorAndroid="transparent"
+        onChangeText={(text) => setEmail(text)}
+        keyboardType='email-address'
+        value={email}
+      />
+      <Text style={styles.text}>Senha</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="*********"
+        underlineColorAndroid="transparent"
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry={true}
+        value={password}
+      />
+      <View style={styles.areaBtn}>
+        <TouchableOpacity 
+          style={styles.handleLogin}
+          onPress={() => {}}
+        >
+          <Text style={styles.textBtn}>Acessar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={goToCadastro}
+        >
+          <Text style={{textAlign: 'center', fontSize: 20, marginTop: 15}}>Criar uma conta</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fffafa',
+    paddingTop: 20,
+    paddingHorizontal: 20
+  },
+  text:{
+    fontSize: 22
+  },
+  input:{
+    marginVertical: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#171614',
+    borderRadius: 5,
+    backgroundColor: '#FFF',
+    height: 50
+  },
+  handleLogin:{
+    marginTop: 10,
+    backgroundColor: '#52a6bf',
+    justifyContent: 'center',
+    height: 50,
+    borderRadius: 10
+  },
+  textBtn:{
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 22
+  },
+  
+});
