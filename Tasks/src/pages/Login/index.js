d96aa0e9d00be731040a7cc198ccdedbb05e16bf
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-06-08 08:00:09 -0300

    Created base files

---
+++ Tasks/src/pages/Login/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import firebase from "../../services/firebaseConnection"
+
+
+export default function Login(){
+  return(
+    <View>
+      <Text>Login</Text>
+    </View>
+  )
+}
+
---
+++ Tasks/src/pages/routes.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "./Login";
+import Home from "./Home";
+import Cadastro from "./Cadastro";
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Cadastro" component={Cadastro} />
+
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
