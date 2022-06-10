0ffde5f20581fc0b538f25a601bdba6c49e7ceb3
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-06-10 07:45:57 -0300

    Style tasks

--- Tasks/src/components/TaskList/index.js
+++ Tasks/src/components/TaskList/index.js
@@ -1,8 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from "react-native";
+import { FontAwesome } from '@expo/vector-icons';
 
 export default function TaskList(props){
   return(
-    <Text>{props.data.name}</Text>
+    <View style={styles.container}>
+      <TouchableOpacity>
+        <FontAwesome name="trash-o" size={24} color="white" />
+      </TouchableOpacity>
+      <TouchableWithoutFeedback>
+        <Text style={styles.text}>{props.data.name}</Text>
+      </TouchableWithoutFeedback>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    marginTop: 15,
+    padding: 10,
+    flexDirection: 'row',
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  text:{
+    color: '#FFF',
+    marginLeft: 10
+  }
+})
