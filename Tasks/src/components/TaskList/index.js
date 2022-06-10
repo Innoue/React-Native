ef33b316edaca84a65bceeab972bf047993db433
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-06-10 10:36:02 -0300

    Changed layout

--- Tasks/src/components/TaskList/index.js
+++ Tasks/src/components/TaskList/index.js
@@ -5,12 +5,14 @@ import { FontAwesome } from '@expo/vector-icons';
 export default function TaskList({data, deleteItem, editItem}){
   return(
     <View style={styles.container}>
-      <TouchableOpacity onPress={() => deleteItem(data.key)}>
-        <FontAwesome name="trash-o" size={24} color="white" />
-      </TouchableOpacity>
-      <TouchableWithoutFeedback onPress={() => editItem(data)}>
-        <Text style={styles.text}>{data.name}</Text>
+      <TouchableWithoutFeedback  onPress={() => editItem(data)}>
+        <View style={styles.textTask}>
+          <Text style={styles.text}>{data.name}</Text>
+        </View>
       </TouchableWithoutFeedback>
+      <TouchableOpacity style={styles.trashTask} onPress={() => deleteItem(data.key)}>
+        <FontAwesome name="trash-o" size={20} color="white" />
+      </TouchableOpacity>
     </View>
   )
 }
@@ -18,15 +20,28 @@ export default function TaskList({data, deleteItem, editItem}){
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-    marginTop: 15,
-    padding: 10,
+    marginTop: 10,
+    marginHorizontal: 5,
     flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  textTask:{
+    backgroundColor: '#292d33',
+    padding: 15,
     borderRadius: 10,
-    alignItems: 'center'
+    marginRight: 10,
+    flex: 1
+  },
+  trashTask:{
+    backgroundColor: '#6b0700',
+    width: '15%',
+    padding: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
   },
   text:{
     color: '#FFF',
-    marginLeft: 10
   }
 })
