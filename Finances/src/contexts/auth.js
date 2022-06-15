25055b610b7ae7807aed011d0fa87964ed60c5a0
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2022-06-15 14:06:18 -0300

    Saving user into async storage

--- Finances/src/contexts/auth.js
+++ Finances/src/contexts/auth.js
@@ -1,9 +1,27 @@
-import React, { useState, createContext, useContext } from 'react'
+import React, { useState, createContext, useContext, useEffect } from 'react'
+import { LogBox } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import firebase from '../services/firebaseConnection'
 export const AuthContext = createContext({})
 
+LogBox.ignoreAllLogs(true)
+
 function AuthProvider({ children }){
   const [user, setUser] = useState(null)
+
+  useEffect(()=>{
+    async function getUser(){
+      await AsyncStorage.getItem('auth_user')
+      .then((value)=>{
+        let dataUser = JSON.parse(value)
+        setUser(dataUser)
+      })
+      .catch((err)=>{
+        console.log('Async Storage error ' + err.code)
+      })
+    }
+    getUser()
+  },[])
 
   async function signUp(email, password, name){
     await firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -13,13 +31,19 @@ function AuthProvider({ children }){
         name: name,
         balance: 0
       })
-      .then((value)=>{
+      .then(async (value)=>{
         let data={
           name: name,
           email: email,
           uid: uid
         }
-        setUser(data)
+        await AsyncStorage.setItem('auth_user', JSON.stringify(data))
+        .then(()=>{
+          setUser(data)
+        })
+        .catch((err)=>{
+          console.log('Async Storage error ' + err.code)
+        })
       })
       .catch((err)=>{
         console('Firebase error ' + err.code)
@@ -35,13 +59,19 @@ function AuthProvider({ children }){
     .then(async (value) => {
       let uid = value.user.uid
       await firebase.database().ref('users').child(uid).once('value')
-      .then((snapshot)=>{
+      .then(async (snapshot)=>{
         let data={
           uid: uid,
           name: snapshot.val().name,
           email: value.user.email,
         }
-        setUser(data)
+        await AsyncStorage.setItem('auth_user', JSON.stringify(data))
+        .then(()=>{
+          setUser(data)
+        })
+        .catch((err)=>{
+          console.log('Async Storage error ' + err.code)
+        })
       })
       .catch((err)=>{
         console('Firebase error ' + err.code)
