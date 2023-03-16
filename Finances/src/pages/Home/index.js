aac21184896e191f9fcd0441f4acd5a48e1dad79
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2023-03-16 11:28:57 -0300

    Added user display name

--- Finances/src/pages/Home/index.js
+++ Finances/src/pages/Home/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 
 import {Background, Container, TextName, TextBalance, TextTitle, List} from './styles'
-
+import {AuthContext} from "../../contexts/auth"
 import RecordsList from "../../components/RecordsList";
 
 export default function Home(){
@@ -12,10 +12,12 @@ export default function Home(){
     {key: '4', type:'receita', value:1022},    
     {key: '5', type:'receita', value:100.12},    
   ])
+
+  const {user} = useContext(AuthContext)
   return (
     <Background>
       <Container>
-        <TextName>Rodrigo</TextName>
+        <TextName>{user && user.name}</TextName>
         <TextBalance>123</TextBalance>
       </Container>
       <TextTitle>Últimas movimentações</TextTitle>
